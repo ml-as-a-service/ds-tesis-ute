c75ff39d3cbe7cbaa07ad3525eaff9bf46023d49
Mario Garcia <mariogarcia.ar@gmail.com>
2021-07-13 03:31:44 -0300

    remove process

--- parse_html/init.js
+++ parse_html/init.js
@@ -7,8 +7,8 @@
 var s = document.createElement("script"); 
 s.src = "https://code.jquery.com/jquery-3.6.0.min.js"; 
 s.onload = function(e){
-    System.init(config);    
-    System.process();
+    System.init(config);    // init Config.data System.data
+
     document.mgrun();
 };  
 document.head.appendChild(s);  
